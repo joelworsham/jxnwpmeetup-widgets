1301e3a797c8bbb9a8972e47b955ca9cea93f7b6
joelworsham <joelworsham@gmail.com>
2014-10-02 18:43:13 -0400

    Step 2

--- jxnwpmeetup.php
+++ jxnwpmeetup.php
@@ -4,9 +4,20 @@
  * Plugin Name: Jaxon WP Meetup Widgets
  * Description: Show some peeps what these WP widgets are all about!
  * Author: Joel Worsham
- * Author URI: http://joelworsham.com
+ * Author URI: http://joelworsham.cStepom
  * Version: 1.0.0
  */
 
 // Include our widget class file
 include_once( 'inc/class-jxnwpmeetupwidget.php' );
+
+// Hook our function into the "widget_init" action
+add_action( 'widgets_init', 'add_widgets' );
+
+/**
+ * Registers all of our widgets
+ */
+function add_widgets() {
+
+	register_widget( 'JxnWPMeetupWidget' );
+}
